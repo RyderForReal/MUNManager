8eade66c700bfa4525bd171c707e2c1b0ca3eafc
RyderForNow <ryder@ryderg.net>
2022-10-26 11:52:35 +0200

    more optimizations (broken)
    Took 24 minutes

--- Views/UnmoderatedCaucusView.axaml.cs
+++ Views/UnmoderatedCaucusView.axaml.cs
@@ -8,15 +8,19 @@ using MUNManager.Configuration;
 using MUNManager.Utils;
 
 namespace MUNManager.Views {
-	public partial class UnmoderatedCaucusView : UserControl {
+	public partial class UnmoderatedCaucusView : UserControl, ICountdownView {
 		internal static UnmoderatedCaucusView Instance = null!;
-		private readonly Timer _timer = new(1000);
+		public Timer Timer { get; } = new(1000);
 
 		private readonly uint _defaultGlobalTime = HomeView.UnmoderatedDuration;
-		private uint _globalTimeLeft;
 
-		private readonly ProgressBar _globalCountdownBar;
-		private readonly Label _globalCountdownText;
+		public bool GlobalTimerRunning { get; set; }
+		public uint GlobalTimeLeft { get; set; }
+		public ProgressBar GlobalProgressBar { get; }
+		// ReSharper disable once InconsistentNaming
+		public Label GlobalTimeLeft_Label { get; }
+		public TextBlock ViewTitle { get; }
+		
 		private readonly Button _globalButton;
 		public UnmoderatedCaucusView()
 		{
@@ -24,53 +28,36 @@ namespace MUNManager.Views {
 			Instance = this;
 			MainWindow.Instance.Title = $"{VolatileConfiguration.EventName} | Unmoderated Caucus";
 
-			_globalTimeLeft = Instance._defaultGlobalTime;
+			GlobalTimeLeft = Instance._defaultGlobalTime;
 			
-			_timer.Elapsed += TimerOnElapsed;
+			Timer.Elapsed += TimerOnElapsed;
 			_globalButton = this.FindControl<Button>("GlobalStartStop");
 
-			_globalCountdownBar = this.FindControl<ProgressBar>("GlobalCountdownBar");
-			_globalCountdownText = this.FindControl<Label>("GlobalCountdownText");
-			_globalCountdownBar.Maximum = _defaultGlobalTime;
-			_globalCountdownBar.Value = _globalTimeLeft;
-			_globalCountdownText.Content = $"{_globalTimeLeft}s left";
+			GlobalProgressBar = this.FindControl<ProgressBar>("GlobalCountdownBar");
+			GlobalTimeLeft_Label = this.FindControl<Label>("GlobalTimeLeft_Label");
+			GlobalProgressBar.Maximum = _defaultGlobalTime;
+			GlobalProgressBar.Value = GlobalTimeLeft;
+			GlobalTimeLeft_Label.Content = $"{GlobalTimeLeft}s left";
 		}
 
 		// TODO: Merge into single method to clean up this mess
 		private void TimerOnElapsed(object? sender, ElapsedEventArgs e)
 		{
-			_globalTimeLeft--;
-			if (_globalTimeLeft == 0)
-			{
-				_timer.Stop();
-			}
-			Dispatcher.UIThread.Post(() =>
-			{
-				_globalCountdownBar.Value = _globalTimeLeft;
-				_globalCountdownText.Content = $"{_globalTimeLeft}s left";
-				if (_globalTimeLeft > _defaultGlobalTime * 0.25)
-				{
-					_globalCountdownBar.Foreground = Brushes.Green;
-					_globalCountdownText.Foreground = Brushes.Green;
-				} else if (IfUtils.IsWithinBounds((int)_globalTimeLeft, (int)_defaultGlobalTime, 0.2, 0.25))
-				{
-					_globalCountdownBar.Foreground = Brushes.YellowGreen;
-					_globalCountdownText.Foreground = Brushes.YellowGreen;
-				} else if (IfUtils.IsWithinBounds((int)_globalTimeLeft, (int)_defaultGlobalTime, 0.15, 0.2))
-				{
-					_globalCountdownBar.Foreground = Brushes.Yellow;
-					_globalCountdownText.Foreground = Brushes.Yellow;
-				}  else if (IfUtils.IsWithinBounds(_globalTimeLeft, _defaultGlobalTime, 0.1, 0.05))
-				{
-					_globalCountdownBar.Foreground = Brushes.Orange;
-					_globalCountdownText.Foreground = Brushes.Orange;
-				} else if (_globalTimeLeft < _defaultGlobalTime * 0.05)
-				{
-					_globalCountdownBar.Foreground = Brushes.Red;
-					_globalCountdownText.Foreground = Brushes.Red;
-				}
+			if (!GlobalTimerRunning) return;
+			
+			GlobalTimeLeft--;
+			var color = CountdownUtils.DetermineColor(GlobalTimeLeft, _defaultGlobalTime);
 
-			});
+			if (color == VolatileConfiguration.CriticalBrush)
+			{
+				CountdownUtils.SetCountdownUIColor(this, color, 1);
+			}
+			
+			if (GlobalTimeLeft == 0)
+			{
+				GlobalTimerRunning = false;
+			}
+			
 		}
 
 		private void InitializeComponent()
@@ -80,19 +67,19 @@ namespace MUNManager.Views {
 
 		private void AllStartStop_Click(object? sender, RoutedEventArgs e)
 		{
-			if (_timer.Enabled)
+			if (Timer.Enabled)
 			{
 				_globalButton.Content = "Start Global";
-				_timer.Stop();
-				_globalCountdownBar.Foreground = Brushes.White;
-				_globalCountdownText.Foreground = Brushes.White;
-				_globalCountdownText.Content = $"{_globalTimeLeft}s left (Paused)";	
+				Timer.Stop();
+				GlobalCountdownBar.Foreground = Brushes.White;
+				GlobalTimeLeft_Label.Foreground = Brushes.White;
+				GlobalTimeLeft_Label.Content = $"{GlobalTimeLeft}s left (Paused)";	
 			}
 			else
 			{
-				_timer.Start();
+				Timer.Start();
 				_globalButton.Content = "Pause Global";
-				_globalCountdownText.Content = $"{_globalTimeLeft}s left";	
+				GlobalTimeLeft_Label.Content = $"{GlobalTimeLeft}s left";	
 			}
 		}
 
